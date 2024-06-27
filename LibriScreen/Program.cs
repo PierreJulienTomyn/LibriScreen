a6ab3175bb7748d08a41e433049bc8cbb1eeaa75
PierreJulienTomyn <tomyn.pierre.dev@gmail.com>
2024-06-27 18:12:05 +0200

    chore: update Program.cs to integrate all components

--- LibriScreen/Program.cs
+++ LibriScreen/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LibriScreen.Services;
+using LibriScreen.UI;
+using LibriScreen.Interfaces;
+
+/// <summary>
+/// Classe principale du programme qui initialise les composants nécessaires et lance l'interface utilisateur.
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// Point d'entrée principal du programme.
+    /// </summary>
+    /// <param name="args">Arguments de la ligne de commande.</param>
+    static void Main(string[] args)
+    {
+        // Création d'une instance de stockage de données utilisant le format JSON,
+        // spécifiant le chemin du fichier où les données sont enregistrées.
+        IDataStorage dataStorage = new JsonDataStorage("Data/mediaData.json");
+
+        // Création du gestionnaire de médias, reliant le stockage de données au système de gestion.
+        IMediaManager mediaManager = new MediaManager(dataStorage);
+
+        // Initialisation de l'interface utilisateur avec le gestionnaire de médias.
+        UserInterface ui = new UserInterface(mediaManager);
+
+        // Démarrage de l'interface utilisateur et affichage du menu principal.
+        ui.ShowMainMenu();
+    }
+}
